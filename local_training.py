39ba038f5df53cd75b982d376976ae94d062cf00
ryancondones <ryannjr2004@gmail.com>
2025-04-14 13:31:02 -0400

    Good

--- local_training.py
+++ local_training.py
@@ -10,7 +10,9 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 def setup(rank, world_size):
     os.environ['MASTER_ADDR'] = '20.81.150.5'  # IP of master node
     os.environ['MASTER_PORT'] = '29500'
+    print(f"[Rank {rank}] Setting up process group...")
     dist.init_process_group("gloo", rank=rank, world_size=world_size)
+    print("ok")
 
 def cleanup():
     dist.destroy_process_group()
